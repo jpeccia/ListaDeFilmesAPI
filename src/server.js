3fbd6265d3a2e2db3201756121cd6df07d6c68ff
João Otávio Peccia <joaootaviopeccia0@@gmail.com>
2025-02-04 14:33:55 -0300

    feat: create server.js

---
+++ src/server.js
@@ -0,0 +1,20 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import helmet from "helmet";
+import rateLimit from "express-rate-limit";
+
+dotenv.config();
+const app = express();
+
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+
+const limiter = rateLimit({ windowMs: 15 * 60 * 1000, max: 100 });
+app.use(limiter);
+
+app.use(`/filme`);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`🔥 Servidor rodando na porta ${PORT}`));
