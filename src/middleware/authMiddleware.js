63f888a901a328d789fe0464e807f256cde33271
João Otávio Peccia <joaootaviopeccia0@@gmail.com>
2025-02-04 14:12:46 -0300

    feat: create auth Middleware

---
+++ src/middleware/authMiddleware.js
@@ -0,0 +1,16 @@
+import dotenv from `dotenv`;
+dotenv.config();
+
+const auth = (req, res, next) => {
+    const authHeader = req.headers.authorization;
+    const user = process.env.ADMIN_USER;
+    const password = process.env.ADMIN_PASSWORD;
+    const expectedAuth = `Basic ${Buffer.from(`${user}:${password}`).toString('base64')}`;
+
+    if (!authHeader || authHeader !== expectedAuth) {
+        return res.status(401).json({ mensagem: `Não autorizado`})
+    }
+    next();
+}
+
+export default auth;
