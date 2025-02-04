0729c6d93420beb0bde0c916bab798366783a926
João Otávio Peccia <joaootaviopeccia0@@gmail.com>
2025-02-04 15:16:18 -0300

    feat: add AdicionarFilme in FilmeController

--- src/controllers/filmeController.js
+++ src/controllers/filmeController.js
@@ -4,29 +4,41 @@ import { v4 as uuidv4 } from "uuid";
 
 export const adicionarFilme = async (req, res) => {
   const { titulo } = req.body;
-  if (!titulo)
+
+  if (!titulo) {
     return res
       .status(400)
       .json({ mensagem: "O título do filme é obrigatório." });
+  }
 
-  const filmeInfo = await buscarFilmeTMDB(titulo);
-  if (!filmeInfo)
-    return res.status(404).json({ mensagem: "Filme não encontrado na TMDB." });
+  try {
+    const filmeInfo = await buscarFilmeTMDB(titulo);
 
-  const novoFilme = new FilmeModel({
-    id_filme: uuidv4(),
-    titulo: filmeInfo.title,
-    sinopse: filmeInfo.overview,
-    ano: filmeInfo.release_date
-      ? filmeInfo.release_date.split("-")[0]
-      : "Desconhecido",
-    genero: filmeInfo.genre_ids[0],
-    estado: "A assistir",
-    historico: [{ acao: "Filme adicionado", timestamp: new Date() }],
-  });
+    if (!filmeInfo) {
+      return res
+        .status(404)
+        .json({ mensagem: "Filme não encontrado na TMDB." });
+    }
 
-  await novoFilme.save();
-  res.status(201).json(novoFilme);
+    const novoFilme = new FilmeModel({
+      id_filme: uuidv4(),
+      titulo: filmeInfo.title,
+      sinopse: filmeInfo.overview,
+      ano: filmeInfo.release_date
+        ? filmeInfo.release_date.split("-")[0]
+        : "Desconhecido",
+      genero: filmeInfo.genre_ids[0],
+      estado: "A assistir",
+      historico: [{ acao: "Filme adicionado", timestamp: new Date() }],
+    });
+
+    await novoFilme.save();
+    res.status(201).json(novoFilme);
+  } catch (error) {
+    res
+      .status(500)
+      .json({ mensagem: "Erro ao adicionar filme", erro: error.message });
+  }
 };
 
 export const atualizarEstado = async (req, res) => {
