26597aef0704cae1ccd09eeb784083a86d131449
João Otávio Peccia <joaootaviopeccia0@@gmail.com>
2025-02-04 14:53:36 -0300

    feat: update log model and add type

--- src/models/logModel.js
+++ src/models/logModel.js
@@ -1,14 +1,18 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 const logSchema = new mongoose.Schema({
-    metodo: { type: String, required: true },
-    url: { type: String, required: true },
-    status: { type: Number, required: true },
-    responseTime: { type: String, required: true },
-    timestamp: { type: Date, default: Date.now },
-    idFilme: { type: String, default: null }, // Vinculado a filmes (se aplicável)
-    mensagem: { type: String, default: '' }
+  tipo: {
+    type: String,
+    enum: ["request", "error", "performance"],
+    required: true,
+  },
+  metodo: String,
+  url: String,
+  status: Number,
+  responseTime: String,
+  mensagem: String,
+  timestamp: { type: Date, default: Date.now },
 });
 
-const LogModel = mongoose.model('Log', logSchema);
+const LogModel = mongoose.model("Log", logSchema);
 export default LogModel;
