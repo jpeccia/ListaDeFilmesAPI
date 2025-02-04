b2d5839d87d93ee2f04f0ed2c14fdb5c7d666c97
João Otávio Peccia <joaootaviopeccia0@@gmail.com>
2025-02-04 14:48:43 -0300

    feat: config log middleware

--- src/middleware/logMiddleware.js
+++ src/middleware/logMiddleware.js
@@ -1,11 +1,30 @@
-import logger from "../config/logger.js";
+import logger, { salvarLogNoBanco } from "../config/logger.js";
 
-const logMiddleware = (req, res, next) => {
-  logger.info({
-    metodo: req.method,
-    url: req.originalUrl,
-    timestamp: new Date(),
+const logMiddleware = async (req, res, next) => {
+  const start = process.hrtime(); // Captura o tempo inicial da requisição
+
+  res.on("finish", async () => {
+    const duration = process.hrtime(start);
+    const responseTime = `${(duration[0] * 1000 + duration[1] / 1e6).toFixed(
+      2
+    )}ms`;
+
+    // Tenta obter o ID do filme, se aplicável
+    const idFilme = req.params.id || (req.body && req.body.idFilme) || null;
+
+    const logData = {
+      metodo: req.method,
+      url: req.originalUrl,
+      status: res.statusCode,
+      responseTime,
+      idFilme,
+      mensagem: "Requisição registrada",
+    };
+
+    logger.info(logData);
+    await salvarLogNoBanco(logData);
   });
+
   next();
 };
 
