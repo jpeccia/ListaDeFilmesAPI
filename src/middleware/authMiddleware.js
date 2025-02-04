d0efd6c329ab4b5ebacfd708f915ee1baf09fee0
João Otávio Peccia <joaootaviopeccia0@@gmail.com>
2025-02-04 15:05:20 -0300

    build: install packages

--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -1,16 +1,19 @@
-import dotenv from `dotenv`;
+import dotenv from "dotenv";
+
 dotenv.config();
 
 const auth = (req, res, next) => {
-    const authHeader = req.headers.authorization;
-    const user = process.env.ADMIN_USER;
-    const password = process.env.ADMIN_PASSWORD;
-    const expectedAuth = `Basic ${Buffer.from(`${user}:${password}`).toString('base64')}`;
+  const authHeader = req.headers.authorization;
+  const user = process.env.ADMIN_USER;
+  const password = process.env.ADMIN_PASSWORD;
+  const expectedAuth = `Basic ${Buffer.from(`${user}:${password}`).toString(
+    "base64"
+  )}`;
 
-    if (!authHeader || authHeader !== expectedAuth) {
-        return res.status(401).json({ mensagem: `Não autorizado`})
-    }
-    next();
-}
+  if (!authHeader || authHeader !== expectedAuth) {
+    return res.status(401).json({ mensagem: `Não autorizado` });
+  }
+  next();
+};
 
 export default auth;
