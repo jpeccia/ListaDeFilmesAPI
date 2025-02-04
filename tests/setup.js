f6d108cda4f00c8862ae528b3476b15a1878355e
João Otávio Peccia <joaootaviopeccia0@@gmail.com>
2025-02-04 20:35:41 -0300

    feat: create setup integration test

---
+++ tests/setup.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config({ path: ".env.test" });
+
+beforeAll(async () => {
+  await mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+});
+
+afterAll(async () => {
+  await mongoose.connection.dropDatabase();
+  await mongoose.connection.close();
+});
