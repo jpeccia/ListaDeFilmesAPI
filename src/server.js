782cea364cc9e8f15a11791964f1a661df5e99b5
João Otávio Peccia <joaootaviopeccia0@@gmail.com>
2025-02-04 14:49:12 -0300

    feat: config server.js with logs and db connection

--- src/server.js
+++ src/server.js
@@ -1,12 +1,22 @@
 import express from "express";
 import dotenv from "dotenv";
+import mongoose from "mongoose";
 import cors from "cors";
 import helmet from "helmet";
 import rateLimit from "express-rate-limit";
-import logMiddleware from "./middleware/logMiddleware";
+import logMiddleware from "./middleware/logMiddleware.js";
+import filmeRoutes from "./routes/filmeRoutes.js";
 
 dotenv.config();
 const app = express();
+
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("✅ Conectado ao MongoDB"))
+  .catch((err) => console.error("Erro ao conectar ao MongoDB:", err));
 
 app.use(helmet());
 app.use(cors());
@@ -16,7 +26,7 @@ app.use(logMiddleware);
 const limiter = rateLimit({ windowMs: 15 * 60 * 1000, max: 100 });
 app.use(limiter);
 
-app.use(`/filme`);
+app.use("/filme", filmeRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`🔥 Servidor rodando na porta ${PORT}`));
